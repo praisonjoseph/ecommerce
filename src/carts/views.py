9ecf811ca25b5306870d8b1dfb1f4b10a824a1fc
Praison <praison.joseph@gmail.com>
2018-10-12 11:30:51 +0530

    billing profile added for checkout

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-
+from accounts.forms import LoginForm, GuestForm
 from products.models import Product
 from .models import Cart
 from orders.models import Order
+from billing.models import BillingProfile
+from accounts.models import GuestEmail
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
@@ -33,5 +35,23 @@ def checkout_home(request):
         return redirect("cart:home")
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
-    return render(request, "carts/checkout.html", {"object": order_obj})
+    user = request.user
+    billing_profile=None
+    login_form = LoginForm()
+    guest_form = GuestForm()
+    guest_email_id = request.session.get('guest_email_id')
+    if user.is_authenticated():
+        print(user.email)
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
+    elif guest_email_id is not None:
+        guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+        billing_profile, billing_guest_profile_created = BillingProfile.objects.get_or_create(email=guest_email_obj.email)
+    else:
+        pass
+    context = {
+        "object": order_obj,
+        "billing_profile": billing_profile,
+        "login_form": login_form
+        }
+    return render(request, "carts/checkout.html", context)
 
